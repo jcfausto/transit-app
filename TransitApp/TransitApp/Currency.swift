bd9e6b935ad966e429951122f50461ecc84eb140
Julio Cesar Fausto <jcfausto@gmail.com>
2016-02-23 23:49:55 -0300

    Currency and TravelMode models with their initial specs

---
+++ TransitApp/TransitApp/Currency.swift
@@ -0,0 +1,20 @@
+//
+//  Currency.swift
+//  TransitApp
+//
+//  Created by Julio Cesar Fausto on 23/02/16.
+//  Copyright © 2016 Julio Cesar Fausto. All rights reserved.
+//
+
+import Foundation
+
+
+class Currency {
+    
+    var symbol: String = "None"
+    
+    init(aSymbol: String) {
+        self.symbol = aSymbol
+    }
+    
+}
---
+++ TransitApp/TransitApp/TravelMode.swift
@@ -0,0 +1,19 @@
+//
+//  TravelMode.swift
+//  TransitApp
+//
+//  Created by Julio Cesar Fausto on 23/02/16.
+//  Copyright © 2016 Julio Cesar Fausto. All rights reserved.
+//
+
+import Foundation
+
+class TravelMode {
+    
+    var mode: String = "None"
+    
+    init(aMode: String) {
+        self.mode = aMode
+    }
+    
+}
