08b1bd9cd2d222b0d39ff8824a9ad7fc7b5a3850
Julio Cesar Fausto <jcfausto@gmail.com>
2016-02-24 23:35:01 -0300

    Added Curry to use with Argo to decode json into models

---
+++ TransitApp/TransitApp/TransitAppError.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorHelper.swift
+//  TransitApp
+//
+//  Created by Julio Cesar Fausto on 24/02/16.
+//  Copyright © 2016 Julio Cesar Fausto. All rights reserved.
+//
+
+import Foundation
+
+enum TransitAppError: ErrorType {
+    case JSONDecodingError(errorMessage: String)
+}
